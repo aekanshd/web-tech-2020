5ae758f9e3b37bc8d5554afe97820ffef24caeb0
Varun Gupta <varungupta3009@gmail.com>
2020-04-15 19:11:40 +0530

    Add Prices

--- codename-gaben/src/components/Detail.js
+++ codename-gaben/src/components/Detail.js
@@ -12,26 +12,29 @@ class Detail extends Component {
 	// }
 
 	get renderBooks() {
-		let book = <h1 className="mt-6 mb-6 text-center leading-9 text-3xl text-gray-500">Prices</h1>;
 		axios.get('http://localhost:62020/api/v1/book/moredetails?q=9782266139816')
 			.then(function (response) {
 				// state.book = response.data;
 				// state.loaded = false;
+				document.querySelector(".item").innerHTML += response.data
 				console.log(response.data);
 			})
 			.catch(function (error) {
 				console.log(error);
 			})
-		if (this.state.book) {
-			book = this.state.book
-		}
-		return book;
+		// if (this.state.book) {
+		// 	// book = this.state.book
+		// }
+		// return book;
 	}
 
 	render() {
 		return (
 			<div>
-				{this.renderBooks}
+				<h1 className="mt-6 mb-6 text-center leading-9 text-3xl text-gray-500">Prices</h1>
+				<div className="item min-h-full flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+					{this.renderBooks}
+				</div>
 			</div>
 		);
 	}
--- codename-gaben/src/components/Profile.js
+++ codename-gaben/src/components/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { search } from "../utils";
+
 
 class Profile extends Component {
 	state = {
